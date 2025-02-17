848b6e5decd588c2d7e575106dd41bbb18a2e797
Aboliina98 <abolfzlismailnjadmypc@gmail.com>
2025-02-17 18:45:47 +0330

    update w

--- w.sh
+++ w.sh
@@ -1,2 +1,15 @@
-weather=$(curl wttr.in/rudsar)
-echo "${weather}"
+#!/bin/bash
+read -p "Full screen and ENTER . . ." reg
+if [[ -z "$reg" ]]
+then
+    clear
+    read -p "Your location : " loc
+    weather=$(curl wttr.in/$loc)
+    echo "${weather}"
+elif [[ "$reg" == "0" ]]
+then
+    echo "programm colsed . "
+    exit 0
+else
+    echo ""
+fi
